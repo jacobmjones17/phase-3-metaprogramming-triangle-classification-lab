4f60c3666ca0ef711573b537ef3bd37656082591
Jacob <jacobmjones17@gmail.com>
2022-10-24 15:28:47 -0400

    commit tree 8436f489735857722f4be9446f59e77dc6ec600f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  
+  attr_reader :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def valid_triangle
+    sides_array = [@side1, @side2, @side3]
+    if sides_array.any? { |side| side <= 0 }
+      raise TriangleError
+    elsif @side1 >= @side2 + @side3 || @side2 >= @side1 + @side3 || @side3 >= @side1 + @side2
+      raise TriangleError
+    end
+  end
+
+  def kind
+    valid_triangle
+    if @side1 == @side2 && @side2 == @side3
+      :equilateral
+    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+      :isosceles
+    else :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
